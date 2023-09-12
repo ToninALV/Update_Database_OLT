362b5d84e3e73851214dbbc1a64cc2ac47b4426f
Antonio Silva <silvag.antoniol@gmail.com>
2023-09-11 23:39:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ hostname = ["BDP-CEN-01-BNG-005","BET-GUA-01-BNG-003","CEM-TLP-01-BNG-009","CLU-
 ips = ["177.73.193.38","177.73.193.32","177.73.193.48","177.73.193.11","177.73.193.35","177.73.193.0","177.73.193.3","177.73.193.6","177.73.193.26"]
 
 port = "6422"
-username = "antonio.silva"
-password = "An@13606973659"
+username = "administrator"
+password = "AS@@28198@2k22!8*C0r3#"
+
+#path01 = "C:\Python\Colect_Clients_to_F1A_per_vlan\comando.txt"
+#path02 = "C:\Python\Colect_Clients_to_F1A_per_vlan\clientes.txt" 
+
+path01 = "C:\Python\Update_Database_OLT\comando.txt"
+path02 = "C:\Python\Update_Database_OLT\clientes.txt"
+
+try:
+    os.remove(path01)
+except:
+    pass
 
 def menu ():
     print("""
@@ -84,7 +95,7 @@ def get_vlans():
 def conect_equipament(command):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, port=port, username=username, password=password)
+    ssh.connect(menu(), port=port, username=username, password=password)
     print("-----CONEXÃO ESTABELECIDA-----")
     
     #open_shell = ssh.invoke_shell()
@@ -97,11 +108,38 @@ def conect_equipament(command):
     #clientes = open_shell.recv(65535).decode('utf-8')
     clientes = str(stdout)
     
-    
-
     ssh.close()
     print("CONEXÃO FINALIZADA")
     return clientes
 
-menu()
-get_vlans()
+def get_clients():
+    list_vlans = get_vlans()
+    print(list_vlans)
+    pevlan = list_vlans[0]
+    cevlan_list = list_vlans[1]
+    i = 0
+    while i != len(cevlan_list):  
+        cevlan = cevlan_list[i]
+        #terminal_lenght = 'mmi-mode enable\n'
+        #command = f'display access-user pevlan {pevlan} cevlan {cevlan} verbose | include name | no-more\n'
+        command = f"""
+            mmi-mode enable\n
+            display access-user pevlan {pevlan} cevlan {cevlan} verbose | include name | no-more\n
+        """
+        clientes = conect_equipament(command)
+
+        with open(path01, 'w') as arquivo:
+            arquivo.write(clientes)
+
+        with open(path02, 'a') as clientes_txt:
+            clientes_txt.write(f'\nVLAN {cevlan}\n\n')
+        arquivo = open(path01, 'r')
+        for line in arquivo:
+            if ("User name") in line:
+                pppoe = line.split()
+                pppoe = pppoe[3]
+                with open(path02, 'a') as clientes_txt:
+                    clientes_txt.write(f'{pppoe}\n')       
+        i += 1
+
+get_clients()
