c1c2614d5aed22a554c554b76a3aea357e744485
Antonio Silva <silvag.antoniol@gmail.com>
2023-10-02 16:59:42 -0300

    Range add
    Substituído IF and ELSE por match case, e adicionado a possibilidade de solicitar range de vlans.

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import time
 import paramiko
 import os
 
-hostname = ["BDP-CEN-01-BNG-005","BET-GUA-01-BNG-003","CEM-TLP-01-BNG-009","CLU-CEN-01-BNG-014","CPO-ANT-01-BNG-004","IRP-CAN-01-BNG-005","LPT-CEN-01-BNG-004","PDS-JARF-01-BNG-007","SDT-CEN-01-BNG-005"]
-
+hostname = ["BDP-CEN-01-BNG-005","BET-GUA-01-BNG-003","CEM-TLP-01-BNG-009","DVL-VSA-01-BNG-014","CPO-ANT-01-BNG-004","IRP-CAN-01-BNG-005","LPT-CEN-01-BNG-004","PDS-JARF-01-BNG-007","SDT-CEN-01-BNG-005"]
 ips = ["177.73.193.38","177.73.193.32","177.73.193.48","177.73.193.11","177.73.193.35","177.73.193.0","177.73.193.3","177.73.193.6","177.73.193.26"]
 
 port = "6422"
@@ -31,7 +30,7 @@ def menu ():
 (1) BOM DESPACHO
 (2) BETIM
 (3) TELEPORTO
-(4) CLÁUDIO
+(4) DIVINÓPOLIS
 (5) CAMPO BELO
 (6) IGARAPÉ
 (7) LAGOA DA PRATA
@@ -40,55 +39,65 @@ def menu ():
 """)
 
     host = int(input("Selecione o local de autenticação: "))
+    match host:
+        case 1:
+            host = ips[0]
+            print(f"Você selecionou {hostname[0]}!")
+        case 2:
+            host = ips[1]
+            print(f"Você selecionou {hostname[1]}!")
+        case 3:
+            host = ips[2]
+            print(f"Você selecionou {hostname[2]}!")
+        case 4:
+            host = ips[3]
+            print(f"Você selecionou {hostname[3]}!")
+        case 5:
+            host = ips[4]
+            print(f"Você selecionou {hostname[4]}!")
+        case 6:
+            host = ips[5]
+            print(f"Você selecionou {hostname[5]}!")
+        case 7:
+            host = ips[6]
+            print(f"Você selecionou {hostname[6]}!")
+        case 8:
+            host = ips[7]
+            print(f"Você selecionou {hostname[7]}!")
+        case 9:
+            host = ips[8]
+            print(f"Você selecionou {hostname[8]}!")
+        case _:
+            print("Opção Inválida, Tente Novamente!!!")
+            return menu()
 
-    if host == 1:
-        host = ips[0]
-        
-        print(f"Você selecionou {hostname[0]}!")
-    elif host == 2:
-        host = ips[1]
-
-        print(f"Você selecionou {hostname[1]}!")
-    elif host == 3:
-        host = ips[2]
-
-        print(f"Você selecionou {hostname[2]}")
-    elif host == 4:
-        host = ips[3]
-
-        print(f"Você selecionou {hostname[3]}!")
-    elif host == 5:
-        host = ips[4]
-
-        print(f"Você selecionou {hostname[4]}!")
-    elif host == 6:
-        host = ips[5]
-
-        print(f"Você selecionou {hostname[5]}!")
-    elif host == 7:
-        host = ips[6]
-
-        print(f"Você selecionou {hostname[6]}!")
-    elif host == 8:
-        host = ips[7]
-
-        print(f"Você selecionou {hostname[7]}!")
-    elif host == 9:
-        host = ips[8]
-
-        print(f"Você selecionou {hostname[8]}!")
-
-
-    return(host)
+    return host
 
 def get_vlans():
     cevlan_list = []
+
     pevlan = input("Digite a PEVLAN: ")
     if pevlan.isdigit():
         pass
     else:
         print("Valor digitado não é válido, Tente Novamente!")
         pass
+
+    option = input("Deseja inserir um range de VLANS ? (S/N): ").upper()
+    if option == "S":
+        vlan_inicial = int(input("Digite a primeira VLAN: "))
+        vlan_final = int(input("Digite a última VLAN: "))
+        range = int(input("Digite a quantidade de Saltos por VLAN:: "))
+        vlan = vlan_inicial
+        while vlan <= vlan_final:
+            cevlan_list.append(vlan)
+            vlan = vlan + range    
+        list_vlans = [pevlan, cevlan_list]
+
+        return list_vlans    
+    elif option == "N":
+        pass
+
     cevlan = input("Digite a CEVLAN: ")
     if cevlan.isdigit():
         cevlan_list.append(cevlan)
@@ -114,7 +123,6 @@ def get_vlans():
 
     return list_vlans
 
-#RESOLVER CADA CEVLAN ELE PUXA A FUNÇÃO DE MENU NOVAMENTE#
 def conect_equipament(command, host):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
